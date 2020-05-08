a936467b16cdb69903c1c9320b775cbcf0163cf9
ravaee <ravaeimohamad@gmail.com>
2020-05-08 20:02:48 +0430

    update-database-author-category

---
+++ DAL/Migrations/20200508152641_Category-Author.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace loppinja.Migrations
+{
+    public partial class CategoryAuthor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Article",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Article",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    Biography = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Author_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Article_AuthorId",
+                table: "Article",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Article_CategoryId",
+                table: "Article",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Author_UserId",
+                table: "Author",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Article_Author_AuthorId",
+                table: "Article",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Article_Category_CategoryId",
+                table: "Article",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Article_Author_AuthorId",
+                table: "Article");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Article_Category_CategoryId",
+                table: "Article");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Article_AuthorId",
+                table: "Article");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Article_CategoryId",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Article");
+        }
+    }
+}
